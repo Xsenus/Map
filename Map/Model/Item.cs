d36ff6925bfc1ecb333d3911211cb6b2edb20425
Илья <ilel@list.ru>
2019-07-05 17:45:26 +0700

    Добавьте файлы проекта.

---
+++ Map/Model/Item.cs
@@ -0,0 +1,26 @@
+﻿namespace Map.Model
+{
+    class Item<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+
+        public Item(TKey key, TValue value)
+        {
+            // TODO: проверка!
+
+            Key = key;
+            Value = value;
+        }
+
+        public override int GetHashCode()
+        {
+            return Key.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
